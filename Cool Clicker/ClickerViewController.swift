6dc76b121002e795e58409e5d3379c312c48da6a
BrandedMilk <59407577+BrandedMilk@users.noreply.github.com>
2020-08-22 11:00:41 +0800

    Added Counter label and tap gesture recogniser.

--- Cool Clicker/ClickerViewController.swift
+++ Cool Clicker/ClickerViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ClickerViewController: UIViewController {
 
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
