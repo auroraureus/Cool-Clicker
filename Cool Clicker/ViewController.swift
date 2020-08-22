baf7fe193106e82051684fa04898fd6bf2f8b04f
auroraureus <69704853+auroraureus@users.noreply.github.com>
2020-08-22 10:29:00 +0800

    Initial Commit

---
+++ Cool Clicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cool Clicker
+//
+//  Created by Nicole Li on 22/8/20.
+//  Copyright © 2020 sleepy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
