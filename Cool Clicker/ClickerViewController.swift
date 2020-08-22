5258e638800ce4956f0820501291f46f23f4dc7b
auroraureus <69704853+auroraureus@users.noreply.github.com>
2020-08-22 11:46:44 +0800

    added the timer (code challenge 2) :D

--- Cool Clicker/ClickerViewController.swift
+++ Cool Clicker/ClickerViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class ClickerViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     var counter = 0
+    var time: Float = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
     }
-
+    
+    @objc
+    func update(){
+       time += 1
+    }
+    
+    @IBAction func tapped(_ sender: Any) {
+        counter += 1
+        label.text = "\(counter)"
+        
+        if counter == 30 {
+            label.text = "done in \(time)s :D"
+        }
+        
+    }
 
 }
 
